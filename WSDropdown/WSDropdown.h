52af96e4abed4b76444e2fd9450b3bba51daa5c3
Yalcin Ozdemir <yalcinozdemir2@gmail.com>
2019-10-29 16:54:43 -0400

    project setup

---
+++ WSDropdown/WSDropdown.h
@@ -0,0 +1,19 @@
+//
+//  WSDropdown.h
+//  WSDropdown
+//
+//  Created by Yalcin on 2019-10-29.
+//  Copyright © 2019 Windscribe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WSDropdown.
+FOUNDATION_EXPORT double WSDropdownVersionNumber;
+
+//! Project version string for WSDropdown.
+FOUNDATION_EXPORT const unsigned char WSDropdownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WSDropdown/PublicHeader.h>
+
+
---
+++ WSDropdownExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WSDropdownExample
+//
+//  Created by Yalcin on 2019-10-29.
+//  Copyright © 2019 Windscribe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
